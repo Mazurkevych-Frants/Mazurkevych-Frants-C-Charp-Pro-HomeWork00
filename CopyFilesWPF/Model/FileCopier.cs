131fa52d4f7aabea43f59c2576acb69ed59053f3
Mazurkevych-Frants <tofer052001@gmail.com>
2025-04-23 14:16:10 +0300

    Игнорируем временные файлы из .vs

---
+++ CopyFilesWPF/Model/FileCopier.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CopyFilesWPF.Model
+{
+    public class FileCopier
+    {
+        private readonly Grid _gridPanel;
+        private readonly FilePath _filePath;
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        public delegate void ProgressChangeDelegate(double progress, ref bool cancel, Grid gridPanel);
+        public delegate void CompleteDelegate(Grid gridPanel);
+        public event ProgressChangeDelegate OnProgressChanged;
+        public event CompleteDelegate OnComplete;
+
+        public bool CancelFlag = false;
+        public ManualResetEvent PauseFlag = new(true);
+
+        public FileCopier(
+            FilePath filePath,
+            ProgressChangeDelegate onProgressChange,
+            CompleteDelegate onComplete,
+            Grid gridPanel)
+        {
+            OnProgressChanged += onProgressChange;
+            OnComplete += onComplete;
+            _filePath = filePath;
+            _gridPanel = gridPanel;
+        }
+
+        public void CopyFile()
+        {
+            CancellationToken cancellationToken = _cancellationTokenSource.Token;
+
+            byte[] buffer = new byte[1024 * 1024];
+            bool continueCopy = true;
+
+            while (continueCopy)
+            {
+                try
+                {
+                    using (var source = new FileStream(_filePath.PathFrom, FileMode.Open, FileAccess.Read))
+                    {
+
+                        var fileLength = source.Length;
+                        using var destination = new FileStream(_filePath.PathTo, FileMode.CreateNew, FileAccess.Write);
+                        long totalBytes = 0;
+                        int currentBlockSize = 0;
+
+                        while ((currentBlockSize = source.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            if (cancellationToken.IsCancellationRequested)
+                            {
+                                File.Delete(_filePath.PathTo);
+                                return;
+                            }
+
+                            totalBytes += currentBlockSize;
+                            double percentage = totalBytes * 100.0 / fileLength;
+                            destination.Write(buffer, 0, currentBlockSize);
+
+                            OnProgressChanged(percentage, ref CancelFlag, _gridPanel);
+
+                            Thread.CurrentThread.Suspend();
+                        }
+                    }
+
+                    continueCopy = false;
+                }
+                catch (IOException error)
+                {
+                    if (!cancellationToken.IsCancellationRequested)
+                    {
+                        var result = MessageBox.Show(error.Message + " Replace?", "Replace?", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                        continueCopy = result == MessageBoxResult.Yes;
+
+                        if (continueCopy)
+                        {
+                            File.Delete(_filePath.PathTo);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(error.Message + " Copying was canceled!", "Cancel", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                        continueCopy = false;
+                        File.Delete(_filePath.PathTo);
+                    }
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message, "Error occurred!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+            OnComplete(_gridPanel);
+        }
+    }
+}
---
+++ CopyFilesWPF/Presenter/MainWindowPresenter.cs
@@ -0,0 +1,172 @@
+﻿using CopyFilesWPF.Model;
+using CopyFilesWPF.View;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace CopyFilesWPF.Presenter
+{
+    public class MainWindowPresenter : IMainWindowPresenter
+    {
+        private readonly IMainWindowView _mainWindowView;
+        private readonly MainWindowModel _mainWindowModel;
+
+        public MainWindowPresenter(IMainWindowView mainWindowView)
+        {
+            _mainWindowView = mainWindowView;
+            _mainWindowModel = new MainWindowModel();
+        }
+
+        public void ChooseFileFromButtonClick(string path) => _mainWindowModel.FilePath.PathFrom = path;
+
+        public void ChooseFileToButtonClick(string path) => _mainWindowModel.FilePath.PathTo = path;
+
+        public void CopyButtonClick()
+        {
+            PrepareFilePaths();
+            var panel = CreateFileCopyPanel(Path.GetFileName(_mainWindowModel.FilePath.PathFrom));
+            AddPauseButton(panel);
+            _mainWindowView.MainWindowView.MainPanel.Children.Add(panel);
+        }
+
+        public void CanselButtonClick()
+        {
+            PrepareFilePaths();
+            var panel = CreateFileCopyPanel(Path.GetFileName(_mainWindowModel.FilePath.PathFrom));
+            AddCancelButton(panel);
+            _mainWindowView.MainWindowView.MainPanel.Children.Add(panel);
+            _mainWindowModel.CopyFile(ProgressChanged, ModelOnComplete, panel);
+        }
+
+        private void PrepareFilePaths()
+        {
+            _mainWindowModel.FilePath.PathFrom = _mainWindowView.MainWindowView.FromTextBox.Text;
+            _mainWindowModel.FilePath.PathTo = _mainWindowView.MainWindowView.ToTextBox.Text;
+            _mainWindowView.MainWindowView.FromTextBox.Clear();
+            _mainWindowView.MainWindowView.ToTextBox.Clear();
+            _mainWindowView.MainWindowView.Height += 60;
+        }
+
+        private Grid CreateFileCopyPanel(string fileName)
+        {
+            var panel = new Grid { Height = 60 };
+            DockPanel.SetDock(panel, Dock.Top);
+
+            panel.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(320) });
+            panel.ColumnDefinitions.Add(new ColumnDefinition());
+            panel.ColumnDefinitions.Add(new ColumnDefinition());
+            panel.RowDefinitions.Add(new RowDefinition { Height = new GridLength(20) });
+            panel.RowDefinitions.Add(new RowDefinition());
+
+            var fileLabel = new TextBlock
+            {
+                Text = fileName,
+                Margin = new Thickness(5, 0, 5, 0)
+            };
+            Grid.SetRow(fileLabel, 0);
+            Grid.SetColumn(fileLabel, 0);
+            panel.Children.Add(fileLabel);
+
+            var progressBar = new ProgressBar
+            {
+                Margin = new Thickness(10)
+            };
+            Grid.SetRow(progressBar, 1);
+            panel.Children.Add(progressBar);
+
+            return panel;
+        }
+
+        private void AddPauseButton(Grid panel)
+        {
+            var pauseButton = new Button
+            {
+                Content = "Pause",
+                Margin = new Thickness(5),
+                Tag = panel
+            };
+            pauseButton.Click += PauseClick;
+            Grid.SetRow(pauseButton, 1);
+            Grid.SetColumn(pauseButton, 1);
+            panel.Children.Add(pauseButton);
+        }
+
+        private void AddCancelButton(Grid panel)
+        {
+            var cancelButton = new Button
+            {
+                Content = "Cancel",
+                Margin = new Thickness(5),
+                Tag = panel
+            };
+            cancelButton.Click += CanselClick;
+            Grid.SetRow(cancelButton, 1);
+            Grid.SetColumn(cancelButton, 2);
+            panel.Children.Add(cancelButton);
+        }
+
+        private void PauseClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Button button || button.Tag is not Grid grid || grid.Tag is not FileCopier copier) return;
+
+            button.IsEnabled = false;
+            if (button.Content.ToString() == "Pause")
+            {
+                copier.PauseFlag.Reset();
+                button.Content = "Resume";
+            }
+            else
+            {
+                copier.PauseFlag.Set();
+                button.Content = "Pause";
+            }
+            button.IsEnabled = true;
+        }
+
+        private void CanselClick(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Button button || button.Tag is not Grid grid || grid.Tag is not FileCopier copier) return;
+
+            button.IsEnabled = false;
+            copier.CancelFlag = true;
+        }
+
+        private void ModelOnComplete(Grid panel)
+        {
+            _mainWindowView.MainWindowView.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                (ThreadStart)delegate
+                {
+                    _mainWindowView.MainWindowView.Height -= 60;
+                    _mainWindowView.MainWindowView.MainPanel.Children.Remove(panel);
+                    _mainWindowView.MainWindowView.CopyButton.IsEnabled = true;
+                });
+        }
+
+        private void ProgressChanged(double percentage, ref bool cancelFlag, Grid panel)
+        {
+            _mainWindowView.MainWindowView.Dispatcher.BeginInvoke(DispatcherPriority.Normal,
+                (ThreadStart)delegate
+                {
+                    foreach (var child in panel.Children)
+                    {
+                        switch (child)
+                        {
+                            case ProgressBar progressBar:
+                                progressBar.Value = percentage;
+                                break;
+
+                            case Button button:
+                                if (!button.IsEnabled && (button.Content?.ToString() == "Resume" || button.Content?.ToString() == "Pause"))
+                                {
+                                    button.Content = button.Content.ToString() == "Resume" ? "Pause" : "Resume";
+                                    button.IsEnabled = true;
+                                }
+                                break;
+                        }
+                    }
+                });
+        }
+    }
+}
